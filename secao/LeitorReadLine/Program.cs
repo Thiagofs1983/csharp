7296871defb2aeab50d8be1276c05478f810cce3
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-03 14:32:37 -0300

    readline

---
+++ secao/LeitorReadLine/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _LeitorReadLine   
+{
+    class Program
+    {
+        static void Main()
+        {
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto + " teste ReadLine");
+        }
+    }
+}
