889573094e4ecfc48049fdbbbd81a9cd218231ec
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-03 14:31:13 -0300

    introdução array

---
+++ secao/Array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] nomes = { "Thiago", "Barbara", "Heitor" };
+            Console.WriteLine(nomes[0]);
+            nomes[0] = "Fonseca";
+            Console.WriteLine(nomes[0]);
+        }
+    }
+}
