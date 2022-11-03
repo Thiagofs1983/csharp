7fe6e666c39438944ade108834ef4f00ef18d99d
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-03 13:59:38 -0300

    conversao de numero e verifica typeof

---
+++ secao/conversaoTextoNumero/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _conversaoNumTexto
+{
+    class Program
+    {
+        static void Main()
+        {
+            string NumeroTexto = "30";
+            int Numero = 30;
+            int NumeroConvertido = int.Parse(NumeroTexto);
+
+            Console.WriteLine("30 + 30 = " + (NumeroConvertido + Numero));
+            Console.WriteLine(NumeroTexto.GetType() == typeof(string));
+        }
+    }
+}
