2c2879fd01e0ef85a46ec3d6c44dce1d5cfb1ecc
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-03 14:09:07 -0300

    converter numero

---
+++ secao/ConversaoNumero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _conversaoNumero
+{
+    class Program
+    {
+        static void Main()
+        {
+            short MeuShort = 10;
+            int MeuInt = MeuShort;
+            long MeuLong = MeuInt;
+
+            long MeuLong2 = 10;
+            int MeuInt2 = Convert.ToInt32(MeuLong2);
+            short MeuShort2 = Convert.ToInt16(MeuInt2);
+        }
+    }
+}
