d15d0cdbdfcf1fb674753fce6c59ab5d1125c73a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 17:57:55 -0300

    Lista

---
+++ secao/Lista/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Lista
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> marcas = new List<string>();
+            marcas.Add("Fiat");
+            marcas.Add("VW");
+            marcas.Add("Renault");
+            marcas.Add("Ford");
+
+            foreach(string marca in marcas)
+            {
+                Console.WriteLine(marca);
+            }
+        }
+    }
+}
