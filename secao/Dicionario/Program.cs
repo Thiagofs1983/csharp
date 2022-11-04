c87cffbc92e43d803abdd9fa2910a731f3f4676c
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 18:27:21 -0300

    Dicionario

---
+++ secao/Dicionario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Dicionario
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, int> dicionario = new Dictionary<string, int>();
+            dicionario.Add("T1", 10);
+            dicionario.Add("T2", 20);
+
+            Console.Write(dicionario["T1"]);
+        }
+    }
+}
