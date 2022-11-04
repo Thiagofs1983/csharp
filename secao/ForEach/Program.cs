e7ad81f6278a0c61ff9e78f0360579e67cd97343
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 19:29:20 -0300

    for e foreach

---
+++ secao/ForEach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ForEach
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] nomes = { "Thiago", "Heitor", "Barbara", "Thales" };
+
+            Console.WriteLine("------Repetição com For-------");
+
+            for (int i = 0; i < nomes.Length; i += 1)
+            {
+                Console.WriteLine("Nome: " + nomes[i]);
+            }
+
+            Console.WriteLine("------Repetição com ForEach-------");
+
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine("Nome: " + nome);
+            }
+        }
+    }
+}
