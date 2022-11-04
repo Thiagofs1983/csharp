95ae97b29628c0a3dde9ecc0e5d02cb3e54491f5
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 18:51:42 -0300

    Exception

---
+++ secao/Exception/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Exception
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("digite um numero");
+            try
+            {
+            int numero = int.Parse(Console.ReadLine());
+            } catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.ReadKey();
+        }
+    }
+}
