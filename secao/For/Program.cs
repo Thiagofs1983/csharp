1dcb961b6a95bc859c8580e1db511e5e85648a07
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 19:21:00 -0300

    Laço for

---
+++ secao/For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _For
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] nomes = { "Thiago", "Fonseca", "de", "Souza" };
+            for (int i = 0; i < nomes.Length ; i += 1)
+            {
+                Console.Write(nomes[i].ToUpper() + " ");
+            }
+            Console.WriteLine("---------------------------");
+
+            for (int i = 0; i <= 10; i += 1)
+            {
+                Console.WriteLine("Linha: " + i);
+            }
+        }
+    }
+}
