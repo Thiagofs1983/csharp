7aab45b12523265842f163747d411aab76a4eab6
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 17:51:01 -0300

    Array multidimensional

---
+++ secao/ArrayMultidimensional/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ArrayMultidimensional
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[,] nomes = new string[2, 4];
+            nomes[0, 0] = "Thiago";
+            nomes[0, 1] = "Fonseca";
+            nomes[0, 2] = "de";
+            nomes[0, 3] = "Souza";
+            nomes[1, 0] = "Barbara";
+            nomes[1, 1] = "Ste";
+            nomes[1, 2] = "Ferr";
+            nomes[1, 3] = "Souza";
+
+            string[,] marcas = new string[,] { { "Ferrari", "Chevrolet", "Volvo" }, { "Fiat", "VW", "Porche" } };
+        }
+    }
+}
---
+++ secao/SolucaoArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] marcasFamosas = { "Ferrari", "Google", "Coca-Cola" };
+            string[] nomes = new string[4];
+            nomes[0] = "Thiago";
+            nomes[1] = "Heitor";
+            nomes[2] = "Barbara";
+            nomes[3] = "Thales";
+        }
+    }
+}
