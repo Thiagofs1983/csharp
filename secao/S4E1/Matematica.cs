5a1f7704ae8a246a771bccfc781478b901ed36c7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 17:34:59 -0300

    Desafio exponenciação com classe estatica e for

---
+++ secao/S4E1/Matematica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4E1
+{
+    internal class Matematica
+    {
+        public static int Exponenciacao(int Base, int Exp)
+        {
+            int resultado = 1;
+            for (int i = 1; i <= Exp; i += 1)
+            {
+                resultado *= Base;
+            }
+            return resultado;  
+        }
+    }
+}
---
+++ secao/S4E1/Program.cs
@@ -0,0 +1,22 @@
+﻿using S4E1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _S4E1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int resultado = Matematica.Exponenciacao(5, 3);
+
+            int resultado1 = Matematica.Exponenciacao(2, 3);
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado1);
+        }
+    }
+}
