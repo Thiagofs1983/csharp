58b8a7f06fbde0e7f1e7dee0e45a850ed056a63c
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-04 08:08:04 -0300

    while

---
+++ secao/While/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _boolObject
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (i <= 10)
+            {
+                Console.WriteLine("Valor da variavel " + i);
+                i++;
+            }
+        }
+    }
+}
