aedff5c75dceedb21fb654e55a01fb930ee6c87b
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 15:22:10 -0300

    Remover espaços vazios

---
+++ secao/_RemoverEspaços/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _RemoverEspacos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Texto = "       Olá amigos          ";
+            Console.WriteLine(Texto.TrimStart());
+            Console.WriteLine(Texto.TrimEnd());
+            Console.WriteLine(Texto.Trim());
+        }
+    }
+}
