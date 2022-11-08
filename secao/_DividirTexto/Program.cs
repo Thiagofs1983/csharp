0a6e2624309c2eec434ee74a8048e508dd9891d3
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 15:37:31 -0300

    Dividir texto

---
+++ secao/_DividirTexto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _DividirTexto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string nomes = "Thiago, Barbara e Heitor";
+            string[] separador = { ", ", " e " };
+            string[] listaNomes = nomes.Split(separador, StringSplitOptions.None);
+            foreach (string nome in listaNomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
