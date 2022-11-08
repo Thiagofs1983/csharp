91af8c2461aa7cc39ee4d7e4c7ea5d2776846f25
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 12:30:03 -0300

    sobrecarga de métodos

---
+++ secao/Sobrecarga/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecarga
+{
+    internal class Pessoa
+    {
+        public int Andar()
+        {
+            return 1;
+        }
+
+        public int Andar(int lvl) // Pode ter 2 metodos com o mesmo nome se diferenciando pelos parametros
+        {
+            return Andar() * lvl;
+        }
+
+        public int Correr()
+        {
+            return Andar() * 2;
+        }
+
+        public int Pedalar()
+        {
+            return Correr() * 2;
+        }
+    }
+}
