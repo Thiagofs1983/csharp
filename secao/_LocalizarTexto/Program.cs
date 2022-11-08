1c4c312f9745ab2116478534eaa1401f9c367bb0
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 14:50:28 -0300

    Localizar uma string com indexof

---
+++ secao/_LocalizarTexto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _LocalizarTexto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Texto = "Olá amigos, estou muito feliz de estar aqui com vocês. Desejo a todos um feliz ano novo";
+            string Palavra = "feliz";
+
+            Console.WriteLine("Tamanho do texto: " + Texto.Length);
+            Console.WriteLine("Localizar: " + Texto.IndexOf(Palavra));
+            Console.WriteLine("Localizar: " + Texto.LastIndexOf(Palavra));
+            Console.WriteLine("Localizar: " + Texto.IndexOf(Palavra, 30));
+        }
+    }
+}
