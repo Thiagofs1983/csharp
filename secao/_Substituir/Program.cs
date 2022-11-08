05d775bf5d952c55ab5e9a168269210cdeda441e
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 14:55:01 -0300

    Replace

---
+++ secao/_Substituir/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Substituir
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Texto = "Olá amigos, estou muito feliz de estar aqui com vocês. Desejo a todos um feliz ano novo";
+            string Palavra = "feliz";
+            string TextoSubstituido = Texto.Replace(Palavra, "triste");
+
+            Console.WriteLine(Texto);
+            Console.WriteLine(TextoSubstituido);
+           
+        }
+    }
+}
