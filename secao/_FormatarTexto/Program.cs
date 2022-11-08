5a4e158038fb8fde4880976d0894ca75d4bd2bdb
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-08 15:06:39 -0300

    Formatar Texto usando string.Format

---
+++ secao/_FormatarTexto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _FormatarTexto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string nome = "Thiago";
+            string natal = "natal";
+
+            string TextoFormatado = string.Format("Olá {0}! Feliz {1}!!!", nome, natal);
+
+            Console.WriteLine(TextoFormatado);
+        }
+    }
+}
---
+++ secao/_VerificarSeExiste/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _VerificarSeExiste
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string Texto = "Olá amigos, estou muito feliz de estar aqui com vocês. Desejo a todos um feliz ano novo";
+            string Palavra = "feliz";
+
+            Console.WriteLine("Verificar existencia no inicio: " + Texto.StartsWith("Olá"));
+            Console.WriteLine("Verificar existencia no final: " + Texto.EndsWith("novo"));
+            Console.WriteLine("Verificar existencia: " + Texto.Contains(Palavra));
+        }
+    }
+}
