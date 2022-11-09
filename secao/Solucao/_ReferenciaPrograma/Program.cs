c3c61d20c8eade94d070aa132978201676494f18
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-09 07:51:41 -0300

    Referencia

---
+++ secao/Solucao/_ReferenciaPrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using _Biblioteca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _ReferenciaPrograma
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            CalculoJuros calc = new CalculoJuros();
+            double resultado = calc.Soma(10, 20);
+
+            Console.WriteLine(resultado);
+        }
+    }
+}
