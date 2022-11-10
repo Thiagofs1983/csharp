0d4f8efbed0b123f36c4190e699b530d6bb47009
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-10 15:56:12 -0300

    Auto implement

---
+++ secao/SolucaoIntermediario/_AutoImplement/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _AutoImplement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Thiago Fonseca";
+            Console.WriteLine(pessoa.Nome);
+        }
+    }
+}
