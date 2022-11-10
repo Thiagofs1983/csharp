f9ecd44b528f53bf171985ede3cb2a7e3ac00d0a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-10 15:48:23 -0300

    Encapsulamento

---
+++ secao/SolucaoIntermediario/_Encapsulamento/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Encapsulamento
+{
+    internal class Pessoa
+    {
+        private string Nome;
+        string CEP;
+        string Endereco;
+        string CPF;
+
+        public void SetNome(string nome)
+        {
+            Nome = nome.ToUpper().Trim();
+        }
+
+        public string GetNome()
+        {
+            return Nome;
+        }
+    }
+}
---
+++ secao/SolucaoIntermediario/_Encapsulamento/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _Encapsulamento
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.SetNome("Thiago Fonseca");
+            Console.WriteLine(pessoa.GetNome());
+        }
+    }
+}
