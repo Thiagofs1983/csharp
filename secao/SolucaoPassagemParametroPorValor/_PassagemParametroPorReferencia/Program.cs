42a7e90b197f8aa8c5e3644bc3fb4143a0ee43b2
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-16 15:04:23 -0300

    Passagem de parametro por referencia

---
+++ secao/SolucaoPassagemParametroPorValor/_PassagemParametroPorReferencia/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _PassagemParametroPorReferencia
+{
+    class Program
+    {
+        static void Main()
+        {
+            double valor = 25;
+            Console.WriteLine("Main > var valor: " + valor);
+
+            AlterarValor(ref valor);
+            Console.WriteLine("Main > var valor: " + valor);
+        }
+
+        static void AlterarValor(ref double valor)
+        {
+            Console.WriteLine("AlterarValor > var valor: " + valor);
+            valor += 10;
+            Console.WriteLine("AlterarValor > var valor: " + valor);
+        }
+    }
+}
