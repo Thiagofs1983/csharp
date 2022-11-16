d5d0ab364568338c5cdc0a80f46040b549c22cec
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-16 15:08:42 -0300

    Passagem Parametro Out

---
+++ secao/SolucaoPassagemParametroPorValor/_PassagemParametroOut/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _PassagemParametroOut
+{
+    class Program
+    {
+        static void Main()
+        {
+            double valor = 25;
+            Console.WriteLine("Main > var valor: " + valor);
+
+            AlterarValor(out valor);
+            Console.WriteLine("Main > var valor: " + valor);
+        }
+
+        static void AlterarValor(out double valor)
+        {
+            valor = 10;
+            Console.WriteLine("AlterarValor > var valor: " + valor);
+        }
+    }
+}
