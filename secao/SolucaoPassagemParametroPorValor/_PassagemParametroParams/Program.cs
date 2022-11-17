1deb8510bdcf7a75511b2f2b6a808b420eb85113
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-17 16:08:34 -0300

    Passagem de parametros pela palavra reservada params

---
+++ secao/SolucaoPassagemParametroPorValor/_PassagemParametroParams/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _PassagemParametroParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            VariasEntradas("Thiago", "Heitor", "Barbara");
+        }
+
+        static void VariasEntradas(params string[] nomes)
+        {
+            foreach (string nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
