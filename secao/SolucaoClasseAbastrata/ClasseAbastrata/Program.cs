a9c572f502f57f9f4ae0da295235221bf6d41e65
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-18 09:41:27 -0300

    Classe abstrata

---
+++ secao/SolucaoClasseAbastrata/ClasseAbastrata/Program.cs
@@ -0,0 +1,20 @@
+﻿using ClasseAbastrata.Lib;
+
+namespace ClasseAbstrata
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Veiculo veiculo = new Veiculo(); Gera um erro, pois a classe veículo é abstrata, portanto não pode ser instanciada
+
+            Carro carro = new Carro();
+            carro.Modelo = "Uno";
+            carro.AnoFabricacao = 2007;
+
+            Console.WriteLine(carro.Modelo);
+            Console.WriteLine(carro.AnoFabricacao);
+            Console.WriteLine(carro.Rodas);
+        }
+    }
+}
