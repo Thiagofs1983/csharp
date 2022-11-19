540d06fecc559300a68fdab359e5be1b64b726f7
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-19 18:28:39 -0300

    tratamento de erro

---
+++ secao/SolucaoException/_TratamentoErro/Pessoa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _TratamentoErro
+{
+    internal class Pessoa
+    {
+        private string CEP;
+
+        public void SetCEP(string cep)
+        {
+            int result;
+            if (int.TryParse(cep, out result))
+            {
+                //Valido
+                CEP = cep;
+            } else
+            {
+                throw new Exception("CEP não valido");
+            }
+        }
+
+        public int getCep()
+        {
+            return int.Parse(CEP);
+        }
+    }
+}
---
+++ secao/SolucaoException/_TratamentoErro/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _TratamentoErro
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pessoa pessoa = new Pessoa();
+            try
+            {
+                pessoa.SetCEP("32530060");
+                Console.WriteLine(pessoa.getCep());
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
