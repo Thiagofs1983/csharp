80f06f46fa346a5c8c2340c5546e1c40dde38d24
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-19 17:01:35 -0300

    Exception

---
+++ secao/SolucaoException/_ErroThrows/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _ErroThrows
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Digite o numero 1: ");
+            string digitado = Console.ReadLine();
+
+            if (digitado == "1")
+            {
+                Console.WriteLine("Correto");
+            } else
+            {
+                throw new Exception("O valor digitado não é valido");
+            }
+        }
+    }
+}
