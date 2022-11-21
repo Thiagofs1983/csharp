23a31da9e196cdf18f5e51415eec9ffd5aee23d8
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-11-21 12:19:49 -0300

    Customizar exception e usar a palavra reservada base para chamar o contrutor da classe pai

---
+++ secao/SolucaoException/_CustomizarException/MinhaException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _CustomizarException
+{
+    internal class MinhaException : Exception
+    {
+        string LinhaDoErro;
+        public MinhaException(string linhaDoErro) : base("Minha exception") // base chama o construtor da classe pai
+        {
+            LinhaDoErro = linhaDoErro;
+        }
+    }
+}
---
+++ secao/SolucaoException/_CustomizarException/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _CustomizarException
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                throw new MinhaException("1");
+            } catch (MinhaException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
